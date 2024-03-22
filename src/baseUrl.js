2d2488e52c7d60b54281b32be8a3d2527f48dc00
Onviro-Tech <developer@onvirotech.com>
2024-03-22 17:05:52 +0545

    baseurl

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,9 +1,9 @@
 import Axios from 'axios';
 const axiosBaseURL = Axios.create({
-    baseURL: process.env.baseUrl || "https://travellinknepal.onvirotech.com",
+    baseURL: process.env.baseUrl || "https://nepaltravellink.onvirotech.net/",
 
 });
 export default axiosBaseURL;
-export const baseUrl = process.env.baseUrl || "https://travellinknepal.onvirotech.com";
+export const baseUrl = process.env.baseUrl || "https://nepaltravellink.onvirotech.net/";
 
 // export const baseUrl = process.env.baseUrl || "http://192.168.1.3:8000";
